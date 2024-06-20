999def539a8f7d09db3c6caef74a768e1e0d8378
sunsky89757 <sunsky20120101@163.com>
2024-06-20 18:19:19 +0800

    Update aiModel.vue

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -39,7 +39,8 @@ model:[
             'suno-v3.5',
             'llama3-70b',
             'kokomi',
-            'search-gpts-chat'
+            'search-gpts-chat',
+            'luma-video'
 			],
 			maxToken:4096
 }); 
